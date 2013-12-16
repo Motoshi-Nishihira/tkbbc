06a7c43eb1d5113de1c01f38511f3703e81b3bf0
Motoshi Nishihira <sha7me1on.mach@gmail.com>
2013-12-16 21:23:22 +0900

    Add show spec

--- spec/views/boards/show.html.slim_spec.rb
+++ spec/views/boards/show.html.slim_spec.rb
@@ -1,12 +1,16 @@
 require 'spec_helper'
 
 describe "boards/show" do
+  let(:board) {stub_model(Board, id: 1, title: "first")}
+
   before(:each) do
-    @board = assign(:board, stub_model(Board))
+    @board = assign(:board, board)
   end
 
   it "renders attributes in <p>" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
+
+    assert_select "p", "#{board.id}. title: #{board.title}"
   end
 end
